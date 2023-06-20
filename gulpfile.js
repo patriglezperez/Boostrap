dadcb02fa4eed88e70750f80ff4689d2c9b57a49
Patricia Gonzalez <patriglzperez@gmail.com>
2023-06-20 14:37:32 +0200

    update content

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+
+/**************** CSS task ****************/
+const css = () => {
+  return src('src/assets/scss/* .scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(dest('src/assets/css'))
+}
+exports.default = series(css)
